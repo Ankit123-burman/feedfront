d6f8f9bd0fd0657b20c837ed6f82e530a0734be5
priyanshuburman540@gmail.com <151107409+Ankit123-burman@users.noreply.github.com>
2024-12-31 08:28:58 +0530

    cors bodyparser

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState([])
+  const [name ,setName] = useState("")
+  const [feedback, setFeedback] = useState("")
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/getFeedback')
+    .then((res)=>{
+      setData(res.data)
+      }).catch(() => {
+        console.log("Error")
+        })
+  })
+ 
+  const AddData = () => {
+   axios.post('http://localhost:3000/saveFeedback',{name,feedback})
+      .then(() => {
+        setData([...data, {name,feedback}])
+        setName("")
+        setFeedback("")
+      }).catch(() => {
+        console.log("Error")
+      })
+  }
+
+  const handleDelete = (index) => {
+    const updatedData = data.filter((_, i) => i !== index);
+    setData(updatedData);
+  };
+
+ 
+  return (
+    <>
+      <div className="mainbox">
+        <div className="container">
+          <input value={name}  onChange={(e)=>setName(e.target.value)}   type="text" placeholder="Name" required />
+          <input value={feedback}  onChange={(e)=>setFeedback(e.target.value)}   type="text" placeholder="Feedback" required />
+          <button onClick={AddData} >Submit</button>
+         
+          {data.map((item, index) => (
+            <div key={index}>
+              <p>id:{index}</p>
+              <p>Your name: {item.name}</p>
+              <p>Your Feedback: {item.feedback}</p>
+              <button onClick={() => handleDelete(index)} >Delete</button>
+            </div>
+          ))}
+
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
